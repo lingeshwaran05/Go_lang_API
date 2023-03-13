1f0edb5b078f55a30c4dfbc6ba63aad5a82b3b26
lingeshwaran05 <lingeshkrish08@gmail.com>
2023-03-13 16:37:39 +0530

    Create API

---
+++ mux-api-project/go.mod
@@ -0,0 +1,5 @@
+module github.com/lingeshwaran05/mux-api-project
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ mux-api-project/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
+func main() {
+    r := mux.NewRouter()
+
+    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello World!"))
+    })
+    fmt.Println("Server is starting at port 8080")
+	fmt.Println("http://localhost:8080/")
+    log.Fatal(http.ListenAndServe(":8080", r))
+}
